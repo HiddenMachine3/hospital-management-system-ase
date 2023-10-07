9bcb90a17f45cabdf04a895841c46ce3bc328d44
HiddenMachine(Suhas) <suhashmsocial@gmail.com>
2023-10-07 23:52:43 +0530

    added files

---
+++ README.md
@@ -0,0 +1,2 @@
+# Hospital Management System
+- ASE Group project
---
+++ credits.py
@@ -0,0 +1,45 @@
+from tkinter import *
+
+
+def credits_press(window, fulwidth, fulheight):
+    credit_subwin = Toplevel(window)
+    credit_subwin.geometry("%dx%d" % (fulwidth, fulheight))
+    icon = PhotoImage(file="images/HIMS Icon.png")
+    credit_subwin.iconphoto(False, icon)
+    credit_subwin.title("Credits")
+    Frame(credit_subwin, bg="deep sky blue").place(
+        x=0, y=0, width=fulwidth, height=fulheight
+    )
+
+    Label(
+        credit_subwin,
+        bg="deep sky blue",
+        fg="white",
+        text="BI11-168 _ Suhas",
+        anchor="w",
+        font=("Ariel", 20, "bold"),
+    ).place(x=100, y=100, width=fulwidth - 200, height=50)
+    Label(
+        credit_subwin,
+        bg="deep sky blue",
+        fg="white",
+        text="BI11-205 _ Maanya",
+        anchor="w",
+        font=("Ariel", 20, "bold"),
+    ).place(x=100, y=170, width=fulwidth - 200, height=50)
+    Label(
+        credit_subwin,
+        bg="deep sky blue",
+        fg="white",
+        text="BI11-128 _ Akshay",
+        anchor="w",
+        font=("Ariel", 20, "bold"),
+    ).place(x=100, y=240, width=fulwidth - 200, height=50)
+
+    Label(
+        credit_subwin,
+        bg="deep sky blue",
+        fg="red",
+        text="TEAM 5",
+        font=("Ariel", 30, "bold"),
+    ).place(x=fulwidth / 4 * 3 - 100, y=fulheight / 2 - 50, width=200, height=70)
